314d7848696807f7425d3ae17c4a7bed59dcf94e
고영일 <ykoh728@gmail.com>
2020-07-30 16:41:50 +0900

    make reducer for counter

--- src/modules/counter.ts
+++ src/modules/counter.ts
@@ -34,3 +34,32 @@ type CounterAction =
 	| ReturnType<typeof increaseBy>;
 
 // 이 리덕스 모듈에서 관리 할 상태의 타입을 선언합니다
+type CounterState = {
+	count: number;
+};
+
+// 초기상태를 선언합니다.
+const initialState: CounterState = {
+	count: 0,
+};
+
+// 리듀서를 작성합니다.
+// 리듀서에서는 state와 함수의 반환값이 일치하도록 작성하세요.
+// 액션에서는 우리가 방금 만든 CounterAction을 타입으로 설정합니다.
+function counter(
+	state: CounterState = initialState,
+	action: CounterAction
+): CounterState {
+	switch (action.type) {
+		case INCREASE:
+			return { count: state.count + 1 };
+		case DECREASE:
+			return { count: state.count - 1 };
+		case INCREASE_BY:
+			return { count: state.count + action.payload };
+		default:
+			return state;
+	}
+}
+
+export default counter;
